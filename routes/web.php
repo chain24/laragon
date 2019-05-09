657952b6e6e598c271ee80ee234ebb6c9a14d8f8
chain24 <1398467081@qq.com>
2019-05-09 17:18:41 +0800

    增加create-school控制器路由

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group( function (){
+
+});
+
+Route::get('create-school', 'SchoolController@index')->middleware('master.admin');
