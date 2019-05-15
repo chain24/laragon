442680a7656d19b59cf7fb9f1301d91cbae392b2
chainw <1398467081@qq.com>
2019-05-15 14:56:16 +0800

    设置语言

--- resources/lang/zh-CN/views.php
+++ resources/lang/zh-CN/views.php
@@ -6,5 +6,19 @@
  * Time: 14:08
  */
 return [
-    'school-createschool-schoollist' => '学校列表',
+    'school-createschool-schoollist'            => '学校列表',
+    'school-createschool-adminlist'             => '部门，班级，学期，学生管理',
+    'school-createschool-Name'                  => '学校名',
+    'school-createschool-Code'                  => '学校代码',
+    'school-createschool-About'                 => '学校简介',
+    'school-createschool-Department'            => '部门',
+    'school-createschool-Classes'               => '班级',
+    'school-createschool-Admin'                 => '管理员',
+    'school-createschool-View_Admin'            => '查看管理员',
+    'school-createschool-Create_Admin'          => '新增管理员',
+    'Logout'                                    => '退出登录',
+    'Login'                                     => '登录',
+    'Register'                                  => '注册',
+    'Dashboard'                                 => '仪表盘',
+    'layouts-home-manageschool'                 => '学校管理'
 ];
--- resources/views/master-home.blade.php
+++ resources/views/master-home.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="page-panel-title">Dashboard</div>
+                    <div class="page-panel-title">{{trans('Dashboard')}}</div>
 
                     <div class="panel-body">
-                        <a class="btn btn-danger btn-lg btn-block" href="{{url('create-school')}}" role="button">Manage Schools</a>
+                        <a class="btn btn-danger btn-lg btn-block" href="{{url('create-school')}}" role="button">{{trans('views.layouts-home-manageschool')}}</a>
                     </div>
                 </div>
             </div>
