45f375484996cd0648efcd1883203cf00786cd49
chainw <1398467081@qq.com>
2019-05-24 11:58:03 +0800

    增加教师导出

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\exports\StudentExports;
+use App\exports\TeacherExports;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -13,7 +14,7 @@ class UploadController extends Controller
         if ($request->type == 'student'){
             return Excel::download(new StudentExports($request->year),date('Y').'-students.xlsx');
         }elseif ($request->type == 'teacher'){
-            return Excel::download(new StudentExports($request->year),date('Y').'-students.xlsx');
+            return Excel::download(new TeacherExports($request->year),date('Y').'-teachers.xlsx');
         }
     }
 }
--- app/exports/TeacherExports.php
+++ app/exports/TeacherExports.php
@@ -9,7 +9,48 @@
 namespace App\exports;
 
 
-class TeacherExports
-{
+use App\User;
+use Illuminate\Database\Query\Builder;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
+class TeacherExports implements FromQuery,ShouldAutoSize,WithHeadings
+{
+    private $headings = [
+            'Name',
+            'Email',
+            'Gender',
+            'Teacher Code',
+            'Blood Group',
+            'Phone Number',
+            'Address',
+        ];
+
+    private $year;
+
+    public function __construct(int $year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * @return Builder
+     */
+    public function query()
+    {
+        return User::query()
+            ->select('name','email','gender','student_code','blood_group','phone_number','address')
+            ->bySchool(auth()->user()->school_id)
+            ->where('role','teacher')
+            ->orderBy('name');
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return $this->headings;
+    }
 }
