f9bf0a67e4ffc413ed3b9a11edeb59687111d686
chainw <1398467081@qq.com>
2019-05-30 17:19:51 +0800

    创前后端分离vue的使用

---
+++ app/Http/Requests/ImpersonateUserRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 19.5.24
+ * Time: 15:21
+ */
+
+namespace App\Http\Requests;
+
+
+class ImpersonateUserRequest
+{
+
+}
---
+++ resources/views/profile/impersonate.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 19.5.24
+ * Time: 14:39
+ */
